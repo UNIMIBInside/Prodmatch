3791824631b1858dea7483672d794e447bbe40d7
belerico <belo.fede@outlook.com>
2019-05-13 17:42:35 +0200

    Add training and testing

--- matching.py
+++ matching.py
@@ -3,7 +3,7 @@ import time
 import deepmatcher as dm
 from os import path
 from pandas import pandas
-from ceneje_prodmatch import DATA_DIR
+from ceneje_prodmatch import DATA_DIR, RESULTS_DIR, CACHE_DIR
 from ceneje_prodmatch.scripts.helpers import preprocess
 from ceneje_prodmatch.scripts.helpers.deepmatcherdata import deepmatcherdata
 import io
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     ignore_columns += ['idProduct']
     train, validation, test = dm.data.process(
         path=path.join('ceneje_prodmatch/ceneje_data'),
+        cache=path.join(CACHE_DIR, 'cache', 'rnn_lstm_fasttext_model.pth'),
         train='train.csv',
         validation='validation.csv',
         test='test.csv',
@@ -33,16 +34,20 @@ if __name__ == "__main__":
         label_attr='label',
         left_prefix='ltable_',
         right_prefix='rtable_',
-        check_cached_data=False,
-        auto_rebuild_cache=True,
         pca=False
     )
-    # model = dm.MatchingModel(attr_summarizer='rnn')
-    # model.run_train(
-    #     train,
-    #     validation,
-    #     epochs=10,
-    #     batch_size=16,
-    #     best_save_path='rnn_fasttext_model.pth',
-    #     pos_neg_ratio=3
-    # )
+    model = dm.MatchingModel(
+        attr_summarizer=dm.attr_summarizers.RNN(
+            word_contextualizer='lstm'
+        ),
+        attr_comparator='abs-diff'
+    )
+    model.initialize(train)
+    model.run_train(
+        train,
+        validation,
+        epochs=10,
+        batch_size=16,
+        best_save_path=path.join(RESULTS_DIR, 'rnn_lstm_fasttext_model.pth'),
+    )
+    model.run_eval(test)
