23df18df04c6f94422d50676fad5196418b6263c
belerico <belo.fede@outlook.com>
2019-07-11 20:36:11 +0200

    Get scores for seller offers

--- get_best_matching_prods.py
+++ get_best_matching_prods.py
@@ -16,6 +16,7 @@ from pandas import pandas
 from collections import namedtuple
 from torch.utils.data import Dataset, DataLoader
 from ceneje_prodmatch import DATA_DIR, DEEPMATCH_DIR, RESULTS_DIR, CACHE_DIR, CONFIG_DIR
+from ceneje_prodmatch.src.helper import preprocess
 from ceneje_prodmatch.src.matching.similarity import Similarity, SimilarityDataset, LogisticRegressionModel
 from ceneje_prodmatch.src.matching.runner import Runner
 
@@ -36,7 +37,13 @@ if __name__ == "__main__":
         encoding='utf-8',
         dtype={'idProduct': object, 'idSeller': object,'idSellerProduct': object}
     )
-    seller_offers = seller_offers[['brandSeller', 'nameSeller', 'descriptionSeller']].fillna(value='')
+    seller_offers = preprocess.normalize(
+        seller_offers[['brandSeller', 'nameSeller', 'descriptionSeller']],
+        fillna=True,
+        na_value='',
+        lower=True
+    )
+    old_seller_offers_cols = seller_offers.columns
     seller_offers.columns = ['left_' + col for col in seller_offers.columns]
     print(seller_offers)
 
@@ -46,33 +53,39 @@ if __name__ == "__main__":
         encoding='utf-8',
         dtype={'idProduct': object, 'idSeller': object,'idSellerProduct': object}
     )
-    ceneje_products = ceneje_products[['idProduct', 'brand', 'nameProduct']]
+    ceneje_products = preprocess.normalize(
+        ceneje_products[['idProduct', 'brand', 'nameProduct']],
+        fillna=True,
+        na_value='',
+        lower=True
+    )
     ceneje_products['description'] = ''
-    ceneje_products.columns = ['right_' + col for col in ceneje_products.columns]
+    ceneje_products.columns = ['right_idProduct'] + ['right_' + col for col in old_seller_offers_cols]
     print(ceneje_products)
 
     unlabeled = pandas.DataFrame([
         chain.from_iterable([left_prod, right_prod])
-        for row in seller_offers.iloc[:2, :].itertuples(index=False)
+        for row in seller_offers.iloc[:10, :].itertuples(index=False)
         for left_prod, right_prod in pairUp(row, ceneje_products)
     ], columns=seller_offers.columns.tolist() + ceneje_products.columns.tolist())\
         .reset_index(drop=True).rename_axis('id', axis=0, copy=False)
+    unlabeled.to_csv(path.join(RESULTS_DIR, 'offers.csv'))
 
-    print(unlabeled)
+    # print(unlabeled)
 
     model = dm.MatchingModel(
         attr_summarizer=dm.attr_summarizers.RNN(),
         attr_comparator='abs-diff'
     )
-    # model.initialize(train, device=device)
     model.load_state(
         path.join(RESULTS_DIR, 'models', 'rnn_pos_neg_fasttext_jaccard_new_cat_rand_model.pth'),
         device=device)
     candidate = dm.data.process_unlabeled(
-        unlabeled,
+        path.join(RESULTS_DIR, 'offers.csv'),
         trained_model=model,
         ignore_columns=['right_idProduct'])
     predictions = model.run_prediction(candidate, output_attributes=True, device=device)
+    predictions.to_csv(path.join(RESULTS_DIR, 'offers_preds.csv'))
     print(predictions)
 
     """ parser = argparse.ArgumentParser()
