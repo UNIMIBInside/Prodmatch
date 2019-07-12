3bf8fdbb2ec0375fb883907522451b0ef306bbd7
belerico <belo.fede@outlook.com>
2019-07-12 17:12:27 +0200

    Optimize imports

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ceneje_prodmatch
 
+## Setup
+
+1. `git clone https://github.com/belerico/ceneje_prodmatch`
+2. Move to the project root. In here you find:
+   * 
--- get_best_matching_prods.py
+++ get_best_matching_prods.py
@@ -1,24 +1,14 @@
 import os
-import math
-import nltk
 import json
-import torch
 import argparse
 import logging
 import deepmatcher as dm
-import py_stringmatching as sm
-import torch.optim as optim
-from torch import nn
 from os import path
-from tqdm import tqdm
 from itertools import chain, product
 from pandas import pandas
 from collections import namedtuple
-from torch.utils.data import Dataset, DataLoader
-from ceneje_prodmatch import DATA_DIR, DEEPMATCH_DIR, RESULTS_DIR, CACHE_DIR, CONFIG_DIR
+from ceneje_prodmatch import DATA_DIR, RESULTS_DIR, CONFIG_DIR
 from ceneje_prodmatch.src.helper import preprocess
-from ceneje_prodmatch.src.matching.similarity import Similarity, SimilarityDataset, LogisticRegressionModel
-from ceneje_prodmatch.src.matching.runner import Runner
 
 logging.getLogger('deepmatcher.core')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -45,7 +35,7 @@ if __name__ == "__main__":
     )
     old_seller_offers_cols = seller_offers.columns
     seller_offers.columns = ['left_' + col for col in seller_offers.columns]
-    print(seller_offers)
+    # print(seller_offers)
 
     ceneje_products = pandas.read_csv(
         path.join(DATA_DIR, 'Products_WashingMachine_20190515.csv'),
@@ -61,11 +51,11 @@ if __name__ == "__main__":
     )
     ceneje_products['description'] = ''
     ceneje_products.columns = ['right_idProduct'] + ['right_' + col for col in old_seller_offers_cols]
-    print(ceneje_products)
+    # print(ceneje_products)
 
     unlabeled = pandas.DataFrame([
         chain.from_iterable([left_prod, right_prod])
-        for row in seller_offers.iloc[:10, :].itertuples(index=False)
+        for row in seller_offers.iloc[:20, :].itertuples(index=False)
         for left_prod, right_prod in pairUp(row, ceneje_products)
     ], columns=seller_offers.columns.tolist() + ceneje_products.columns.tolist())\
         .reset_index(drop=True).rename_axis('id', axis=0, copy=False)
@@ -87,42 +77,4 @@ if __name__ == "__main__":
     predictions = model.run_prediction(candidate, output_attributes=True, device=device)
     predictions.to_csv(path.join(RESULTS_DIR, 'offers_preds.csv'))
     print(predictions)
-
-    """ parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-m', 
-        '--model',
-        default=os.path.join(RESULTS_DIR, 'model', 'best_model.pth'), 
-        help='model path', 
-        action='store'
-    )
-    parser.add_argument(
-        '-s',
-        '--seller-offers', 
-        default='0.05', 
-        type=str, 
-        help='sampling interval in seconds', 
-        action='store'
-    )
-    parser.add_argument(
-        '-o', 
-        '--output', 
-        default=RESULTS_DIR, 
-        type=str, 
-        help='path to store results', 
-        action='store'
-    )
-    parser.add_argument(
-        '-f', 
-        '--flatpak', 
-        help='whether run octave from flatpak', 
-        action='store_true'
-    )
-    parser.add_argument(
-        '-r', 
-        '--remove-old-results', 
-        help='whether remove old results folder', 
-        action='store_true'
-    )
-    args = parser.parse_args() """
     
