7286b554109a0d92f29eb13812f4c2fca18085ae
belerico <belo.fede@outlook.com>
2019-10-02 12:38:53 +0200

    Add script to analyze data

---
+++ analyze_data.py
@@ -0,0 +1,65 @@
+import os
+import numpy
+import pandas
+from os import path
+from collections import Counter
+from ceneje_prodmatch import DATA_DIR, UNSPLITTED_DATA_DIR, DEEPMATCH_DIR, CONFIG_DIR
+from ceneje_prodmatch.src.helper import preprocess
+
+def get_null_values_num_by_col(dataset: pandas.DataFrame, col: str, na_value=''):
+    """
+        Get the number of null values in the specified column
+
+        Parameters
+        ----------
+
+        dataset (pandas.DataFrame): the dataset to search in
+        col (str): a string specifing the column 
+
+        Returns
+        -------
+
+        number of null values
+    """
+    assert(col in dataset.columns)
+    return dataset[col].isna().sum()
+
+def get_attr_avg_var_len(dataset: pandas.DataFrame, col: str):
+    """
+        Get the avarage and variance length of the elements in dataset specified column
+
+        Parameters
+        ----------
+
+        dataset (pandas.DataFrame): the dataset to search in
+        col (str): a string specifing the column 
+
+        Returns
+        -------
+
+        tuple containing (average, variance)
+    """
+    assert(col in dataset.columns)
+    lengths = dataset[col].apply(lambda x: numpy.nan if pandas.isna(x) else len(x))
+    # mean = lengths.sum() / (len(dataset) - dataset[col].isna().sum())
+    # print(mean, numpy.nanmean(lengths))
+    # var = lengths.apply(lambda x: (x - mean) ** 2).sum() / (len(dataset) - dataset[col].isna().sum())
+    # print(var, numpy.nanvar(lengths))
+    return numpy.nanmean(lengths), numpy.nanvar(lengths)
+
+def get_most_frequent_words(dataset: pandas.DataFrame, col: str, top_n=10):
+    return pandas.DataFrame(   
+        pandas.Series(' '.join(dataset['descriptionSeller']).split()).value_counts(),
+        columns=['Count']
+    ).rename_axis('Word').iloc[:top_n, :]
+
+if __name__ == '__main__':
+    dataset = pandas.read_csv(
+        path.join(DATA_DIR, 'SellerProductsData_LedTv_20190426.csv'), 
+        sep='\t', 
+        encoding='utf-8', 
+        na_values='')
+    print(get_null_values_num_by_col(dataset, 'brandSeller'))
+    print(get_attr_avg_var_len(dataset, 'brandSeller'))
+    dataset = preprocess.normalize(dataset)
+    print(get_most_frequent_words(dataset, 'descriptionSeller'))
